4e1865f90ad7f67ec459a98a64e7e7949a4ab92e
maria loureiro <mariamadureiraloureiro@gmail.com>
2019-10-28 17:17:00 +0000

    Iterations 1 to 3

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,25 +1,38 @@
 // ITERATION 1
 
+
+
 function updateSubtotal($product) {
-  // ...
+
+  const price = parseFloat($product.querySelector(".price span").innerText);
+  const quantity = $product.querySelector(".quantity input").valueAsNumber;
+  const $tableSubtotal = $product.querySelector(".subtotal span");
+  const $subtotal = price * quantity;
+
+  $tableSubtotal.innerText = $subtotal; 
+ 
+return $subtotal
+ 
 }
+
 
 function calculateAll() {
-  // ITERATION 2
-  //...
-  // ITERATION 3
-  //...
+  let $product = document.getElementsByClassName('product');
+  let table = document.querySelectorAll(".product");
+  let $total = 0;
+  for( i = 0; i < table.length; i++) {
+    
+    $total += updateSubtotal($product) }
+  
 }
 
-const $calculateTrigger = document.getElementById('calculate');
+const $calculateTrigger = document.getElementById("calculate");
 
-$calculateTrigger.addEventListener('click', calculateAll);
+$calculateTrigger.addEventListener("click", calculateAll);
 
 // ITERATION 4
 
-function addProductRemoveListener($removeButton) {
-  // ...
-}
+function addProductRemoveListener($removeButton) {}
 
 // ITERATION 5
 
